36cbc7621b33b8ecf2745b9ae336bbc0d0375b75
José Auyón <83108711+auyjos@users.noreply.github.com>
2021-11-11 15:58:05 -0600

    Add files via upload

---
+++ Ejercicio6Grupal/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author José Auyón
+ * @author Javier Alvarado
+ * @author Francisco
+ *
+ */
+module ejercicio6 {
+	exports org.arapp.model;
+}
---
+++ Ejercicio6Grupal/src/org/arapp/model/SmartPhone.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.arapp.model;
+
+/**
+ * * @author Jose Auyon
+ * @author Javier Alvarado
+ * @author Francisco
+ *
+ *
+ */
+public class SmartPhone extends Device {
+
+	@Override
+	public void makePhoneCall(int phoneNumber) {
+		System.out.println("Llamando al numero: " + phoneNumber + " desde mi: " + this.deviceType + " " + this.getManufacturer());
+
+	}
+
+	@Override
+	public void takePic() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void browseInInternet(String url) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playVideo(String fileName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveDeviceTo(int PosX, int PosY) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
